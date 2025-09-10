46ffd8c5495471e3090140242adb175b243427e3
tatsukawa0353 <seinouke@gmail.com>
2025-09-10 15:31:53 +0900

    sonogram

--- kashika.py
+++ kashika.py
@@ -17,8 +17,18 @@ sampling_rate = 1.0 / (time[1] - time[0])
 # ソノグラムを計算してプロット
 f, t, Sxx = spectrogram(pi, fs=sampling_rate, nperseg=2048, noverlap=1536)
 
+# デシベル(dB)に変換
+db_Sxx = 10 * np.log10(Sxx + 1e-10)
+
+# カラースケールの調整 
+cmap = 'gray_r'
+
+# 2. 表示する色の範囲(ダイナミックレンジ)を決定
+vmax = np.max(db_Sxx)  # データ全体の最大値を取得し、これを一番濃い色(黒)とする
+vmin = vmax - 30       # 最大値から50dB下までを色の範囲とする
+
 plt.figure(figsize=(10, 6))
-plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud', cmap='magma')
+plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud', cmap=cmap, vmin=vmin, vmax=vmax)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
 plt.ylim(0, 10000) # 論文の図に合わせて周波数範囲を設定
