503bf03bec3541c24f06ab09786297e18a346034
tatsukawa0353 <seinouke@gmail.com>
2025-09-16 20:09:06 +0900

    wa

--- kashika_waveform.py
+++ kashika_waveform.py
@@ -9,30 +9,9 @@ from scipy.signal import spectrogram
 df = pd.read_csv('simulation_output_2(a).csv')
 
 time = df['time'].values
-pi = df['pi'].values
+x_left = df['x_left'].values
+x_right = df['x_right'].values
 
-# サンプリング周波数を計算
-sampling_rate = 1.0 / (time[1] - time[0])
 
-# ソノグラムを計算してプロット
-f, t, Sxx = spectrogram(pi, fs=sampling_rate, nperseg=2048, noverlap=1536)
 
-# デシベル(dB)に変換
-db_Sxx = 10 * np.log10(Sxx + 1e-10)
-
-# カラースケールの調整 
-cmap = 'gray_r'
-
-# 2. 表示する色の範囲(ダイナミックレンジ)を決定
-vmax = np.max(db_Sxx)  # データ全体の最大値を取得し、これを一番濃い色(黒)とする
-vmin = vmax - 30       # 最大値から50dB下までを色の範囲とする
-
-plt.figure(figsize=(10, 6))
-plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud', cmap=cmap, vmin=vmin, vmax=vmax)
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.ylim(0, 10000) # 論文の図に合わせて周波数範囲を設定
-plt.title('Spectrogram of Simulated Birdsong (pi)')
-plt.colorbar(label='Intensity [dB]')
-plt.savefig('waveform_2(a).png') 
-print("グラフを waveform_2(a).png という名前で保存しました。")
+#
