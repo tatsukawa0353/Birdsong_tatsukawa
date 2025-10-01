0e04b81d3648da13174759553e2743967c49184e
tatsukawa0353 <seinouke@gmail.com>
2025-10-01 16:44:51 +0900

    2025/10/01 update

--- kashika_fig6b.py
+++ kashika_fig6b.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from scipy.signal import spectrogram
 
 # CSVファイルを読み込む
-df = pd.read_csv('simulation-rk4_output_2(a).csv')
+df = pd.read_csv('simulation-rk4_output_2(b).csv')
 
 time = df['time'].values
 pi = df['pi'].values
@@ -15,7 +15,7 @@ pi = df['pi'].values
 sampling_rate = 1.0 / (time[1] - time[0])
 
 # ソノグラムを計算してプロット
-f, t, Sxx = spectrogram(pi, fs=sampling_rate, window='blackmanharris', nperseg=245760, noverlap=184320) #2(a)は245760　184320
+f, t, Sxx = spectrogram(pi, fs=sampling_rate, window='blackmanharris', nperseg=81920, noverlap=61440) #2(a)は245760　184320
 
 # デシベル(dB)に変換
 db_Sxx = 10 * np.log10(Sxx + 1e-10)
@@ -25,15 +25,15 @@ cmap = 'gray_r'
 
 # 2. 表示する色の範囲(ダイナミックレンジ)を決定
 #vmax = np.max(db_Sxx)  # データ全体の最大値を取得し、これを一番濃い色(黒)とする
-vmax = 85
-vmin = 55       
+vmax = 80
+vmin = vmax - 16       # 2(a) 35
 
 plt.figure(figsize=(10, 10))
 plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud', cmap=cmap, vmin=vmin, vmax=vmax)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
 plt.ylim(0, 10000) # 論文の図に合わせて周波数範囲を設定
-plt.title('Spectrogram of Simulated Birdsong (pi) Fig.6(a)')
-plt.colorbar(label='Intensity [dB]')
-plt.savefig('sonogram-rk4_2(a).png') 
-print("グラフを sonogram-rk4_2(a).png という名前で保存しました。")
+plt.title('Spectrogram of Simulated Birdsong (pi) Fig.6(b)')
+#plt.colorbar(label='Intensity [dB]')
+plt.savefig('sonogram-rk4_2(b).png') 
+print("グラフを sonogram-rk4_2(b).png という名前で保存しました。")
--- kashika_fig6c.py
+++ kashika_fig6c.py
@@ -15,7 +15,7 @@ pi = df['pi'].values
 sampling_rate = 1.0 / (time[1] - time[0])
 
 # ソノグラムを計算してプロット
-f, t, Sxx = spectrogram(pi, fs=sampling_rate, nperseg=81920, noverlap=61440) #2(a)は245760　184320
+f, t, Sxx = spectrogram(pi, fs=sampling_rate, window='blackmanharris', nperseg=81920, noverlap=61440) #2(a)は245760　184320
 
 # デシベル(dB)に変換
 db_Sxx = 10 * np.log10(Sxx + 1e-10)
@@ -25,15 +25,15 @@ cmap = 'gray_r'
 
 # 2. 表示する色の範囲(ダイナミックレンジ)を決定
 #vmax = np.max(db_Sxx)  # データ全体の最大値を取得し、これを一番濃い色(黒)とする
-vmax = 90
-vmin = vmax - 25       # 2(a) 35
+vmax = 80
+vmin = vmax - 16       # 2(a) 35
 
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(10, 10))
 plt.pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud', cmap=cmap, vmin=vmin, vmax=vmax)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
 plt.ylim(0, 10000) # 論文の図に合わせて周波数範囲を設定
-plt.title('Spectrogram of Simulated Birdsong (pi)')
-plt.colorbar(label='Intensity [dB]')
+plt.title('Spectrogram of Simulated Birdsong (pi) Fig.6(c)')
+#plt.colorbar(label='Intensity [dB]')
 plt.savefig('sonogram-rk4_2(c).png') 
 print("グラフを sonogram-rk4_2(c).png という名前で保存しました。")
