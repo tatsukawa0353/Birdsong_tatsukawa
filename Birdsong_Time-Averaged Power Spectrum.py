0842df6740d576b85500d59b6117fc40ab321530
tatsukawa0353 <s.tatsukawa@biomech.me.es.osaka-u.ac.jp>
2025-12-26 16:08:43 +0900

    2025/12/26

--- Birdsong_Time-Averaged Power Spectrum.py
+++ Birdsong_Time-Averaged Power Spectrum.py
@@ -12,10 +12,10 @@ import os
 
 # --- 設定項目 ---
 # 読み込むデータがあるフォルダ
-INPUT_FOLDER = "simulation_results_2_x0=0.02_low parameters epsilon/"
+INPUT_FOLDER = "simulation_results_1_f0=0.4e7_x0=0.02_low epsilon/"
 
 # グラフ画像を保存するフォルダ
-OUTPUT_FOLDER = "psd_graphs_2_x0=0.02_low parameters epsilon_linear/" # フォルダ名を変更
+OUTPUT_FOLDER = "psd_graphs_1_f0=0.4e7_x0=0.02_low wpsilon_linear/" # フォルダ名を変更
 
 # スペクトログラム生成パラメータ
 nperseg = 245760
